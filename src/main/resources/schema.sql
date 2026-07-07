5b047bcb3a6074b41ebd571d5c8d6f67d66b1a2c
DonkeyFish <1687268864@qq.com>
2026-07-07 19:02:12 +0800

    手动修改： 完善数据库建表语句

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS bookmarks (
     icon        TEXT,
     category    TEXT,
     add_date    TEXT,
-    create_time TEXT    NOT NULL,
-    update_time TEXT    NOT NULL
+    create_time TEXT    NOT NULL DEFAULT (datetime('now','localtime')),
+    update_time TEXT    NOT NULL DEFAULT (datetime('now','localtime'))
 );
 
 CREATE INDEX IF NOT EXISTS idx_bookmarks_url ON bookmarks (url);
